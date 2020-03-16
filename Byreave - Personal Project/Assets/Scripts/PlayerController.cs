4f5aaaafe97bd30ba02fd4f4472fef3de52f1611
Byreave Luo <654297651@qq.com>
2020-03-16 00:02:21 -0600

    fishied lab3

--- Byreave - Personal Project/Assets/Scripts/PlayerController.cs
+++ Byreave - Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10.0f;
+    private Rigidbody playerRb;
+    private float zBound = 6.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+
     }
 }
