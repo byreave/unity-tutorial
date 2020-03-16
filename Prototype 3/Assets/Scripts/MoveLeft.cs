2789c893e90eb4931fdb418a96f8eb0915172d44
Byreave Luo <654297651@qq.com>
2020-03-15 19:36:48 -0600

    prototype3

---
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private PlayerController playerControllerScript;
+    private float speed = 3.0f;
+    private float leftBound = -15.0f;
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerControllerScript.gameOver == false)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
